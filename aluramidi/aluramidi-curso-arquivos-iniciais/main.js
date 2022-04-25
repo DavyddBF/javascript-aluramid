248b5b1d18e827eda1bf5a2304480f8a0b806df5
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2022-04-25 19:28:23 -0300

    atualizado

--- aluramidi/aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi/aluramidi-curso-arquivos-iniciais/main.js
@@ -1,8 +1,16 @@
-// Pom
-function tocaSomPom (){
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio){
+    document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onclick = tocaSomPom;
+let contador = 0;
+
+//enquanto
+while (contador < listaDeTeclas.length) {
+    listaDeTeclas[contador].onclick = tocaSom;
+
+    contador++;
+
+    console.log(contador);
+}
