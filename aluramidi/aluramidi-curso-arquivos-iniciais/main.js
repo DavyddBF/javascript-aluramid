d8d67e3f086e0e2ddf9867c3e64725aa488a9365
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2022-05-27 17:35:42 -0300

    atualizado

--- aluramidi/aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi/aluramidi-curso-arquivos-iniciais/main.js
@@ -2,7 +2,7 @@ function tocaSom (seletorAudio){
 
    const elemento = document.querySelector(seletorAudio);
 
-   if (elemento != null && elemento.localName === 'audio') {
+   if (elemento && elemento.localName === 'audio') {
         elemento.play();
    } else {
     console.log('Elemento não encontrado ou seletor inválido');
@@ -25,7 +25,7 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     tecla.onkeydown = function (evento) {
 
-        if (evento.code === 'Space' || evento.code === 'Space'){
+        if (evento.code === 'Space' || evento.code === 'Enter'){
             tecla.classList.add('ativa');
         }
 
